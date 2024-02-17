d5839f4bb47d28f0d4af22e186bfaf7ea5be443c
Rajveer Shaw <122556097+RajveerShaw@users.noreply.github.com>
2024-02-18 03:21:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+document.querySelector("#abt").addEventListener("mouseover",()=>{
+    document.querySelector("#abtdrp").style.display = "inline";
+    document.querySelector("#abt").addEventListener("mouseout",()=>{
+        document.querySelector("#abtdrp").style.display = "none";
+})
+
+document.querySelector("#abtdrp").addEventListener("mouseover",()=>{
+    document.querySelector("#abtdrp").style.display = "inline";
+    document.querySelector("#abtdrp").addEventListener("mouseout",()=>{
+        document.querySelector("#abtdrp").style.display = "none";
+        })
+    })
+})
+
+
+
+
+document.querySelector("#evt").addEventListener("mouseover",()=>{
+    document.querySelector("#evtdrp").style.display = "inline";
+    document.querySelector("#evt").addEventListener("mouseout",()=>{
+        document.querySelector("#evtdrp").style.display = "none";
+})
+
+document.querySelector("#evtdrp").addEventListener("mouseover",()=>{
+    document.querySelector("#evtdrp").style.display = "inline";
+    document.querySelector("#evtdrp").addEventListener("mouseout",()=>{
+        document.querySelector("#evtdrp").style.display = "none";
+        })
+    })
+})
+
+document.querySelector("#ser").addEventListener("mouseover",()=>{
+    document.querySelector("#serdrp").style.display = "inline";
+    document.querySelector("#ser").addEventListener("mouseout",()=>{
+        document.querySelector("#serdrp").style.display = "none";
+})
+
+document.querySelector("#serdrp").addEventListener("mouseover",()=>{
+    document.querySelector("#serdrp").style.display = "inline";
+    document.querySelector("#serdrp").addEventListener("mouseout",()=>{
+        document.querySelector("#serdrp").style.display = "none";
+        })
+    })
+})
